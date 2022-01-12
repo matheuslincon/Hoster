0f89b074e5e2342851efc16261838b0f35d1998f
Matheus Andrade <matheuslincon50@gmail.com>
2022-01-12 10:23:35 -0300

    added left part into header

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,19 @@
+import Image from 'next/image';
+import headerImg from '../public/header.svg';
+
 function Header() {
   return (
-    <header>
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       {/* Left */}
-      <div></div>
+      <div className="flex relative items-center my-auto h-14 cursor-pointer">
+        <Image
+          src={headerImg}
+          layout='fill'
+          alt='hoster'
+          objectFit='contain'
+          objectPosition='left'
+        />
+      </div>
 
       {/* Middle */}
       <div></div>
