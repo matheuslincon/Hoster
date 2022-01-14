636a14768d3e0790c52d0ea791cdb7aa755bf1cc
Matheus Andrade <matheuslincon50@gmail.com>
2022-01-13 21:13:26 -0300

    create handleSelect function

--- components/Header.js
+++ components/Header.js
@@ -12,10 +12,15 @@ function Header() {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
 
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
   const selectionRange = {
     startDate: startDate,
     endDate: endDate,
-    key: 'Selection'
+    key: 'selection'
   }
 
   return (
@@ -57,6 +62,9 @@ function Header() {
         <div>
           <DateRangePicker
             ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#BAA360"]}
+            onChange={handleSelect}
           />
         </div>
       )}
