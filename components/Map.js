5eef80ee40d3daa4bad6c0300fdc644aeb523602
Matheus Andrade <matheuslincon50@gmail.com>
2022-01-16 22:37:57 -0300

    added center to my map

--- components/Map.js
+++ components/Map.js
@@ -3,18 +3,20 @@ import ReactMapGL from 'react-map-gl';
 import getCenter from 'geolib/es/getCenter';
 
 function Map({ searchResults }) {
-  const [viewport, setViewport] = useState({
-    width: '100%',
-    height: '100%',
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8
-  })
-
   const coordinates = searchResults.map(result => ({
     longitude: result.long,
     latitude: result.lat,
-  }))
+  }));
+
+  const center = getCenter(coordinates);
+
+    const [viewport, setViewport] = useState({
+    width: '100%',
+    height: '100%',
+    latitude: center.latitude,
+    longitude: center.longitude,
+    zoom: 8
+  });
 
   return (
     <ReactMapGL
