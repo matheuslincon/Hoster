cc11d28759866fb7bae5dca1bb9766ae0b73ddee
Matheus Andrade <matheuslincon50@gmail.com>
2022-01-16 22:14:35 -0300

    added map styling

--- components/Map.js
+++ components/Map.js
@@ -1,11 +1,20 @@
 import ReactMapGL from 'react-map-gl';
+import { useState } from 'react';
 
 function Map() {
+  const [viewport, setViewport] = useState({
+    width: '100%',
+    height: '100%',
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8
+  })
 
   return (
     <ReactMapGL
       mapStyle='mapbox://styles/akkiscrystal/ckyhz0joh1gpk14l058neobzi'
       mapboxApiAccessToken={process.env.mapbox_key}
+      {...viewport}
     >
 
     </ReactMapGL>
--- pages/search.js
+++ pages/search.js
@@ -44,7 +44,7 @@ function Search({ searchResults }) {
           </div>
         </section>
 
-        <section className="hidden xl:inline-flex">
+        <section className="hidden xl:inline-flex xl:min-w-[600px]">
           <Map />
         </section>
       </main>
